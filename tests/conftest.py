fec2c365b3fc06e2b5d1ea38443ffe21ed931594
Adzirio <77503148+Adzirio@users.noreply.github.com>
2026-04-24 03:24:44 +0700

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,12 @@
 import pytest
-
 from app import create_app
 
 
 @pytest.fixture(scope="session")
 def app():
     app = create_app()
-    app.config.update(TESTING=True, SQLALCHEMY_DATABASE_URL="sqlite:///test.db")
+    app.config.update(TESTING=True,
+                      SQLALCHEMY_DATABASE_URL="sqlite:///test.db")
     return app
 
 
