12ae14be2bcea1c05807180a02cd5737172c80b3
Adzirio <77503148+Adzirio@users.noreply.github.com>
2026-04-24 02:45:15 +0700

    Added project on github
    test push project

---
+++ app/__init__.py
@@ -0,0 +1,24 @@
+from flask import Flask, g
+from contextlib import contextmanager
+from .db import init_db, create_tables, get_session
+from .routes import bp
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object("app.config.Config")
+
+    init_db(app.config["SQLALCHEMY_DATABASE_URL"])
+    create_tables()
+    app.register_blueprint(bp)
+
+    @app.before_request
+    def before_request():
+        g.session = get_session()
+
+    @app.teardown_appcontext
+    def teardown_session(exception=None):
+        if hasattr(g, 'session'):
+            g.session.close()
+
+    return app
